9d533d57036982f7969ae65525daaf98dda3a0cb
Prashantbhalla1 <82282194+Prashantbhalla1@users.noreply.github.com>
2021-04-19 10:52:58 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,11 @@
+
+burg = document.querySelector(".burg");
+g = document.querySelector(".bar");
+h = document.querySelector(".le");
+j = document.querySelector(".ri");
+
+burg.onclick = () => {
+  g.classList.toggle("bar2");
+  h.classList.toggle("hh");
+  j.classList.toggle("hh");
+};
